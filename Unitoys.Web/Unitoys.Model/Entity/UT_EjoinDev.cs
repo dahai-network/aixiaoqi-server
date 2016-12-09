e611bd2fdd29818aeec64a161fe7a9b0963493a4
valen926 <436083@qq.com>
2016-12-09 15:58:52 +0800

    创建了设备表，和设备对应的端口表

---
+++ Unitoys.Web/Unitoys.Model/Entity/UT_EjoinDev.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unitoys.Model
+{
+    /// <summary>
+    /// 一正设备账号信息管理
+    /// </summary>
+    public class UT_EjoinDev : UT_Entity
+    {
+        public UT_EjoinDev()
+        {
+            this.UT_EjoinDevSlot = new HashSet<UT_EjoinDevSlot>();
+         }
+        /// <summary>
+        /// 设备名，注册名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// 最大端口数
+        /// </summary>
+        public int MaxPort { get; set; }
+        /// <summary>
+        /// 注册状态
+        /// </summary>
+        public string RegStatus { get; set; }
+        /// <summary>
+        /// 设备注册IP
+        /// </summary>
+        public string RegIp { get; set; }
+        /// <summary>
+        /// 设备软件版本
+        /// </summary>
+        public string Version { get; set; }
+        /// <summary>
+        /// 设备MAC地址
+        /// </summary>
+        public string Mac { get; set; }
+        /// <summary>
+        /// 设备类型
+        /// </summary>
+        public ModType ModType { get; set; }
+
+        public virtual ICollection<UT_EjoinDevSlot> UT_EjoinDevSlot { get; set; }
+    }
+    public enum ModType
+    {
+        [Description("移动联通设备")]
+        GSM = 0,
+        [Description("电信设备")]
+        WCDMA = 1
+    }
+}
---
+++ Unitoys.Web/Unitoys.Model/Entity/UT_EjoinDevSlot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unitoys.Model
+{
+    /// <summary>
+    /// 设备端口
+    /// </summary>
+    public class UT_EjoinDevSlot : UT_Entity
+    {
+        /// <summary>
+        /// 端口的IMEI码
+        /// </summary>
+        public string IMEI { get; set; }
+        /// <summary>
+        /// 端口号
+        /// </summary>
+        public int PortNum { get; set; }
+        /// <summary>
+        /// SIM卡ICCID
+        /// </summary>
+        public string ICCID { get; set; }
+        /// <summary>
+        /// SIM卡号码
+        /// </summary>
+        public string SimNum { get; set; }
+
+        /// <summary>
+        /// 设备端口状态
+        /// </summary>
+        public DevPortStatus Status { get; set; }
+        public Guid? EjoinDevId { get; set; }
+        public virtual UT_EjoinDev UT_EjoinDev { get; set; }
+
+        public Guid? UserId { get; set; }
+        public virtual UT_Users UT_Users { get; set; }
+    }
+    public enum DevPortStatus
+    {
+        [Description("无SIM卡")]
+        NOSIM = "0",
+        [Description("有SIM卡，没有注册")]
+        NOREG = "1",
+        [Description("SIM卡注册中")]
+        REGING = "2",
+        [Description("SIM卡注册成功")]
+        REGSUCCESS = "3",
+        [Description("正在呼叫")]
+        CALLING = "4",
+        [Description("告警")]
+        WARNING = "5",
+        [Description("注册失败")]
+        REGERROR = "6",
+        [Description("设备锁卡")]
+        DEVLOCKCARD = "7",
+        [Description("运营商锁卡")]
+        OPTLOCKCARD = "8",
+        [Description("读SIM卡错误")]
+        READERROR = "9",
+        [Description("端口被用户禁用")]
+        PORTDISABLE = "A"
+    }
+}
