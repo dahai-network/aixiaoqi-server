4a001b84df73b41882ef9e0dc26aeece19b89cc3
214978335@qq.com <214978335@qq.com>
2017-04-13 18:05:06 +0800

    1.后台一正设备管理端口列表增加排序；2.无空卡序列号获取卡数据时改变算法

--- Unitoys.Web/Unitoys.ESIM_MVNO/SimOp/SimDataSTK.cs
+++ Unitoys.Web/Unitoys.ESIM_MVNO/SimOp/SimDataSTK.cs
@@ -44,8 +44,18 @@ namespace Unitoys.ESIM_MVNO
 
         public string GetData()
         {
-            return simData;
-            //return Convert.ToBase64String(DataHelper.HexToByte(simData));
+            //return simData;
+            return StringToHexString(Convert.ToBase64String(DataHelper.HexToByte(simData)), Encoding.UTF8);
+        }
+        private string StringToHexString(string s, Encoding encode)
+        {
+            byte[] b = encode.GetBytes(s);//按照指定编码将string编程字节数组
+            string result = string.Empty;
+            for (int i = 0; i < b.Length; i++)//逐字节变为16进制字符
+            {
+                result += Convert.ToString(b[i], 16);
+            }
+            return result;
         }
 
         /// <summary>
